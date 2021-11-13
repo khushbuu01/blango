07bfe031ca56d7f7e5d76470e09247cdfca2cbf4
khushbu Ravaria <khushburavaria777@gmail.com>
2021-11-13 15:18:33 +0000

    Finish Introduction and Django Admin

--- blango/urls.py
+++ blango/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path
 import blog.views
+import debug_toolbar
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index),
-    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
+    path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
+    path("ip/", blog.views.get_ip)
 ]
 
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
